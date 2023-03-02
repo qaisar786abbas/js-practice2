c72e6d1286537f15e0aded36a845184d72b08d64
Qaisar Abbas <122449496+qaisar786abbas@users.noreply.github.com>
2023-03-02 07:42:33 -0800

    added files

---
+++ practice-2/app.js
@@ -0,0 +1,77 @@
+// var arr = ["Qaisar","Abbas"]
+// console.log(arr.join("&"));
+// console.log(arr.reverse());
+// console.log(arr.splice(2,0,"Ali"));
+// console.log(arr.splice(0,3));
+
+
+
+// var str = prompt("Enter any word by your choice.");
+// p = str.split("").reverse().join("");
+// if(str == p){
+//     alert("Yes, The word you enter is a polindrome.");
+// }
+// else{
+//     alert("No, The word you enter is not a polindrome.");
+// }
+
+
+
+
+// var pets = ["cat","dog","cow"]
+// console.log(pets);
+// pets[0] = [pets[2],pets[2] = pets[0]][0];
+// console.log(pets);
+// console.log(pets.reverse());
+
+
+
+// var students = [["qaisar","abbas"]["mesum","abdullah"]]
+// console.log(students[1][1][3]);
+
+
+// var students = ["Qaisar","Abbas","Ali","Asim"]
+// console.log(students[0]);
+// console.log(students[1]);
+// console.log(students[2]);
+// console.log(students[3]);
+// console.log(students.reverse());
+
+
+
+
+// var name =  ["qaisar","abbas"]
+// console.log(name.pops());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
